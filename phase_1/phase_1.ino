b4e5c876274474914808400babcfb3a3d813d36c
Charlie Friend <charles.d.friend@gmail.com>
2019-01-29 12:24:30 -0800

    Use exponential averaging for joystick values

--- phase_1/phase_1.ino
+++ phase_1/phase_1.ino
@@ -2,6 +2,8 @@
  * Joystick module.
  */
 
+const float k_joystickAlpha = 0.2;
+
 typedef struct
 {
   int pinX;
@@ -11,6 +13,9 @@ typedef struct
   int posY;
 } Joystick_t;
 
+/**
+ * Initialize a Joystick.
+ */
 void Joystick_init(Joystick_t& joy, int pinX, int pinY)
 {
   pinMode(pinX, INPUT);
@@ -18,12 +23,17 @@ void Joystick_init(Joystick_t& joy, int pinX, int pinY)
 
   joy.pinX = pinX;
   joy.pinY = pinY;
+  joy.posX = analogRead(pinX);
+  joy.posY = analogRead(pinY);
 }
 
+/**
+ * Update joystick values using exponential averaging.
+ */
 void Joystick_update(Joystick_t& joy)
 {
-  joy.posX = analogRead(joy.pinX);
-  joy.posY = analogRead(joy.pinY);
+  joy.posX = (1 - k_joystickAlpha) * analogRead(joy.pinX) + k_joystickAlpha * joy.posX;
+  joy.posY = (1 - k_joystickAlpha) * analogRead(joy.pinY) + k_joystickAlpha * joy.posY;
 }
 
 /**
